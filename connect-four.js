81e1b10261e666595995c810a4f03c1aa456a651
Pierre Guilbault <guilbaultpj@gmail.com>
2021-05-05 16:21:33 -0400

    Finally have a board, differences with removeAttribute and classList.remove

--- connect-four.js
+++ connect-four.js
@@ -2,24 +2,33 @@ import {Game} from "./game.js"
 
     let game = undefined;
     let gameName = document.getElementById("game-name");
+    const clickTarget = document.getElementById("click-targets");
 
     function updateUI() {
         let boardHolder = document.getElementById("board-holder");
         if (game === undefined) {
             boardHolder.setAttribute("class", "is-invisible")
+        } else {
+            boardHolder.classList.remove("is-invisible");
+            gameName.innerHTML = game.getName();
         }
-        if (game !== undefined) {
-            boardHolder.removeAttribute("is-invisibile");
-            gameName.innerHTML = game.whoIs();
-        }
+            if (game.currentPlayer === 1) {
+                clickTarget.removeAttribute("red");
+                clickTarget.setAttribute("class", "black");
+            } else {
+                clickTarget.removeAttribute("black");
+                clickTarget.setAttribute("class", "red");
+            }
     }
 
+    // console.log(Game);
+    // console.log(game);
 window.addEventListener('DOMContentLoaded', event => {
 
     const newGameButton = document.getElementById('new-game');
     const player1 = document.getElementById('player-1-name');
     const player2 = document.getElementById('player-2-name');
-    const clickTarget = document.getElementById("click-targets");
+
 
     function whoIs(player) {
         player.addEventListener('keyup', event => {
@@ -41,8 +50,9 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
     clickTarget.addEventListener("click", event => {
-        game.playInColumn();
+        Game.playInColumn();
         console.log(currentPlayer.value)
+        updateUI();
     });
 
 
